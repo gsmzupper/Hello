71949e8d9bed0ca8bd1bb91624aa768059c2eebc
gsmzupper <gabriel.merconi@zup.com.br>
2025-01-14 10:05:20 -0300

    first commit

---
+++ src/main/java/com/example/Hello/HelloWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWithSpringApplication.class, args);
+	}
+
+}
